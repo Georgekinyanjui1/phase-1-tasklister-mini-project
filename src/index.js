13fc5cdaf368236c2616f64acfffa0cb574e8091
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-17 13:33:40 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+
+    const formTwo = document.getElementById("newform");
+    const taskDiscript = document.getElementById("newdescription");
+    const newPriority = document.getElementById("newtaskdiscri");
+
+    const newdoc = document.getElementById("tasks");
+
+    formTwo.addEventListener("submit", createNewTask);
 });
+const createNewTask = event => {
+    event.preventDefault();
+    //stop this form  not  to submit
+    const taskDiscript = document.getElementById("newdescription");
+    const newTask = document.createElement("li");
+    newTask.innerText = .value;
+
+    appendNewTask(newTask);
+    event.target.reset();
+};
+
+const appendNewTask = task => {
+    document.getElementById("tasks").appendChild(task);
+};
